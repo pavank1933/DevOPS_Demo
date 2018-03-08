49ec894a8f07a7756dc2cbd16158d5bc1caa4b8b
ansumanverma <anshuman.verma@gmail.com>
2018-03-08 19:26:08 +0530

    Update HelloWorldResource.java

--- HelloWorldApp/src/main/java/com/demo/helloworldapp/HelloWorldResource.java
+++ HelloWorldApp/src/main/java/com/demo/helloworldapp/HelloWorldResource.java
@@ -23,7 +23,7 @@ public class HelloWorldResource {
     @GET
     @Produces("text/html")
     public String getXml() {
-        return "<html><body><h1>Hello World Demo"+nameStorage.getName()+"!</h1></body></html>";
+        return "<html><body><h1>Hello World Demo"+"!</h1></body></html>";
     }
 
     /**
